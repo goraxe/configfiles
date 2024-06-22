2012483bcd23c139539adfda1b62377e1499be5d
goraxe <goraxe@gmail.com>
2024-06-21 19:30:10 -0700

    fix(nvim): telescope add keymaps

--- nvim/lua/goraxe/plugins/telescope.lua
+++ nvim/lua/goraxe/plugins/telescope.lua
@@ -1,32 +1,64 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
-    dependencies = { 'nvim-lua/plenary.nvim', "nvim-web-devicons" },
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.7',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        "nvim-web-devicons",
+        "nvim-telescope/telescope-symbols.nvim",
+        "nvim-telescope/telescope-file-browser.nvim",
+    },
     keys = {
-        { '<leader>ff',
-            function() 
+        {
+            '<leader>ff',
+            function()
                 local builtin = require('telescope.builtin')
-                builtin.find_files {} 
-            end, desc = "find files"
+                builtin.find_files {}
+            end,
+            desc = "find files"
         },
-        { '<leader>fr',
-            function() 
+        {
+            '<leader>fr',
+            function()
                 local builtin = require('telescope.builtin')
-                builtin.live_grep{} 
-            end, desc = "live grep"
+                builtin.live_grep {}
+            end,
+            desc = "live grep"
         },
-        { '<leader>fb',
-            function() 
+        {
+            '<leader>fb',
+            function()
                 local builtin = require('telescope.builtin')
-                builtin.buffers{} 
-            end, desc = "find buffers"
+                builtin.buffers {}
+            end,
+            desc = "find buffers"
         },
-        { '<leader>fd',
-            function() 
+        {
+            '<leader>fd',
+            function()
                 local builtin = require('telescope.builtin')
-                builtin.diagnostics{}
-            end, desc = "find diagnostics"
+                builtin.diagnostics {}
+            end,
+            desc = "find diagnostics"
         },
-        { '<leader>t',":Telescope", desc = "open telescope"
+        {
+            '<leader>fh',
+            function()
+                local builtin = require('telescope.builtin')
+                builtin.help_tags {}
+            end,
+            desc = "find help tags"
+        },
+        { '<leader>t', ":Telescope<Cr>", desc = "open telescope"
+        }
+    },
+    opts = {
+        defaults = {
+            color_devicons = true,
+        },
+        extensions = {
+            "telescope-symbols",
+            file_browser = { }
         }
     }
+
 }
