042119db7b400f71a3064927efb3fc59424dae7b
goraxe <goraxe@gmail.com>
2024-06-21 19:28:35 -0700

    fix(nvim): add treesitter-context + refactor

--- nvim/lua/goraxe/plugins/tree-sitter.lua
+++ nvim/lua/goraxe/plugins/tree-sitter.lua
@@ -1,4 +1,3 @@
-
 local M = {
     {
         "nvim-treesitter/nvim-treesitter",
@@ -26,6 +25,16 @@ local M = {
             },
             highlight = { enable = true },
             indent = { enable = true },
+            refactor = {
+                highlight_definitions = { enable = true },
+                highlight_current_scope = { enable = true },
+                smart_rename = {
+                    enable = true,
+                    keymaps = {
+                        smart_rename = "grr",
+                    },
+                }
+            },
             textobjects = {
                 select = {
                     enable = true,
@@ -53,7 +62,7 @@ local M = {
                     -- mapping query_strings to modes.
                     selection_modes = {
                         ['@parameter.outer'] = 'v', -- charwise
-                        ['@function.outer'] = 'V', -- linewise
+                        ['@function.outer'] = 'V',  -- linewise
                         ['@class.outer'] = '<c-v>', -- blockwise
                     },
                     -- If you set this to `true` (default is `false`) then any textobject is
@@ -116,7 +125,7 @@ local M = {
                 },
             },
         },
-        config = function (_, opts)
+        config = function(_, opts)
             local configs = require('nvim-treesitter.configs')
 
             configs.setup(opts)
@@ -127,8 +136,36 @@ local M = {
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
         },
-
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-refactor",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require('treesitter-context').setup {
+                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                line_numbers = true,
+                multiline_threshold = 20, -- Maximum number of lines to show for a single context
+                trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+                -- Separator between context and content. Should be a single character string, like '-'.
+                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+                separator = '-',
+                zindex = 20, -- The Z-index of the context window
+                on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+            }
+        end,
     }
+
 }
 
 return M
