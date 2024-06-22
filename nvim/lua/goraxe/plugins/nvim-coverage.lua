aa8b5079102e90d6ed7d3db9f63f4a0ed729aa5d
goraxe <goraxe@gmail.com>
2024-06-21 19:34:43 -0700

    fix(nvim): change coverage signs text

--- nvim/lua/goraxe/plugins/nvim-coverage.lua
+++ nvim/lua/goraxe/plugins/nvim-coverage.lua
@@ -5,16 +5,17 @@ local M = {
     opts = {
         auto_reload = true,
         commands = true,
-        --highlights = {
-            -- customize highlight groups created by the plugin
-        --    covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
-        --    uncovered = { fg = "#F07178" },
-        --},
+        highlights = {
+           -- customize highlight groups created by the plugin
+           covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
+           uncovered = { fg = "#F07178" },
+        },
         signs = {
             -- use your own highlight groups or text markers
-            covered = { hl = "CoverageCovered", text = " ▎" },
-            uncovered = { hl = "CoverageUncovered", text = " ▎" },
-            partial = { hl = "CoveragePartial", text = " ▎" },
+            covered = { hl = "CoverageCovered", text = "⦚" },
+            uncovered = { hl = "CoverageUncovered", text = "⦚" },
+            partial = { hl = "CoveragePartial", text = "⦚" },
+            priority = 11,
         },
         summary = {
             -- customize the summary pop-up
