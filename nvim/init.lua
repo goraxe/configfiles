d032269d589c0c3b7fc5a2b29b8f03056ea7979f
goraxe <goraxe@gmail.com>
2026-03-02 22:40:06 -0800

    feat(nvim): enable nvim dist choosing via NVIM_DIST env

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
@@ -15,26 +15,31 @@ vim.opt.rtp:prepend(lazypath)
 -- Example using a list of specs with the default options
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
-require("goraxe")
+if vim.env['NVIM_DIST'] == "LazyVim" then
+    vim.opt.rtp:prepend(vim.fn.stdpath("config") .. "/LazyVim")
+    require("config.lazy")
+else
+    require("goraxe")
 
-require("lazy").setup({
-   {
-       "sainnhe/edge",
-       priority = 1000,
-       config = function ()
-           vim.cmd 'colorscheme edge'
-       end
-   },
-  { "folke/neoconf.nvim",
-    cmd = "Neoconf",
-    priority = 1000,
-    lazy = false,
-    config = function(_, opts)
-            require("neoconf").setup(opts)
-    end
-  },
-  { "folke/neodev.nvim" },
-  {{import = "goraxe.plugins"}},
-  { dir = "~/.config/nvim/lua/goraxe/lsp"},
-})
+    require("lazy").setup({
+       {
+           "sainnhe/edge",
+           priority = 1000,
+           config = function ()
+               vim.cmd 'colorscheme edge'
+           end
+       },
+      { "folke/neoconf.nvim",
+        cmd = "Neoconf",
+        priority = 1000,
+        lazy = false,
+        config = function(_, opts)
+                require("neoconf").setup(opts)
+        end
+      },
+      { "folke/lazydev.nvim" },
+      {{import = "goraxe.plugins"}},
+      { dir = "~/.config/nvim/lua/goraxe/lsp"},
+    })
 
+end
